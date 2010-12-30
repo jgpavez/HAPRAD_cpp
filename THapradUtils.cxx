dea4ef1cc471339ddf60bd99e23f7b0e6c614a3e
smancill <smancill@alumnos.inf.utfsm.cl>
2010-12-30 16:32:16 -0300

    Fix some problems in dfint function

--- THapradUtils.cxx
+++ THapradUtils.cxx
@@ -65,97 +65,69 @@ namespace HapradUtils {
     }
 
 
+
     Double_t dfint(Int_t narg, double *arg, Int_t *nent, Double_t *ent, Double_t *table)
     {
-        Double_t kd = 1;
+        Double_t kd = 0;
         Double_t m = 1;
-        Int_t ncomb[narg];
         Int_t ja = 0;
         Int_t jb, k;
-        Int_t i = 0;
+        Int_t ncomb[narg];
         Double_t d[narg];
+        Int_t ient[narg];
         Double_t dfint;
-        while (i <= narg) {
+        Int_t i = 0;
+        while (i < narg) {
             ncomb[i] = 1;
             jb = ja + nent[i]; // VERIFICAR SI LOS LIMITES ESTAN CORRECTOS!!!!
             Int_t j = ja;
-            Int_t jr = 0;
             while (j <= jb) {
-                if (arg[i] <= ent[j]) {
-                    if (j != ja) {
-                        jr = j - 1;
-                        d[i] = (ent[j] - arg[i]) / (ent[j] - ent[jr]);
-                        ent[i] = j - ja;
-                        kd = kd + ent[i] * m;
-                        m = m * nent[i];
-                        ja = jb + 1;
-                        break;
-
-                    } else {
-                        j++;
-                        jr = j - 1;
-                        d[i] = (ent[j] - arg[i]) / (ent[j] - ent[jr]);
-                        ent[i] = j - ja;
-                        kd = kd + ent[i] * m;
-                        m = m * nent[i];
-                        ja = jb + 1;
-                        break;
-
-                    }
-
-                }
+                if (arg[i] <= ent[j]) goto exit_inner_loop;
                 j++;
             }
             j = jb;
-            j++;
-            d[i] = (ent[j] - arg[i]) / (ent[j] - ent[jr]);
-            ent[i] = j - ja;
-            kd = kd + ent[i] * m;
+    exit_inner_loop:
+            if (j == ja) ++j;
+            d[i] = (ent[j] - arg[i]) / (ent[j] - ent[j - 1]);
+            ient[i] = j - ja;
+            kd = kd + ient[i] * m;
             m = m * nent[i];
             ja = jb + 1;
             i++;
         }
+
         dfint = 0;
-        while (1) {
-            Double_t fac = 1.;
-            Int_t iadr = kd;
-            Double_t ifadr = 1.;
-            //cambiado a 0 dado el indice de los arreglos en fortran
-            i = 0;
-            while (i <= narg) {
-                if (ncomb[i] == 0) {
-                    fac = fac * d[i];
-                    iadr  = iadr - ifadr;
-                    ifadr = ifadr * nent[i];
-
-                } else {
-                    fac = fac * (1. -  d[i]);
-                    ifadr = ifadr * nent[i];
-                }
-                i++;
-            }
-            dfint = dfint + fac * table[iadr];
-            Int_t il = narg;
-
-            while (ncomb[il] == 0) {
-                il--;
-                if (il == 0) return dfint;
-            }
-
-            ncomb[il] = 0;
-            if (il == narg) {
-                continue;
+    do_again:
+        Double_t fac = 1.;
+        Int_t iadr = kd;
+        Int_t ifadr = 1;
+        //cambiado a 0 dado el indice de los arreglos en fortran
+        i = 0;
+        while (i < narg) {
+            if (ncomb[i] == 0) {
+                fac = fac * d[i];
+                iadr  = iadr - ifadr;
             } else {
-                il++;
-                k = il;
-                while (k <= narg) {
-                    ncomb[k] = 1;
-                    k++;
-                }
-                continue;
+                fac = fac * (1. -  d[i]);
             }
+            ifadr = ifadr * nent[i];
+            ++i;
         }
-        return dfint;
 
+        dfint = dfint + fac * table[iadr];
+        Int_t il = narg - 1;
+        while (ncomb[il] == 0) {
+            --il;
+            if (il < 0) return dfint;
+        }
+
+        ncomb[il] = 0;
+        if (il == narg) goto do_again;
+        k = il + 1;
+        while (k < narg) {
+            ncomb[k] = 1;
+            k++;
+        }
+        goto do_again;
     }
 }
