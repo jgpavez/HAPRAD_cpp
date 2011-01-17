1411f2fd4e2857f656bc753d0eb7d22d1e724090
smancill <smancill@alumnos.inf.utfsm.cl>
2011-01-17 15:15:26 -0300

    Fix errors in TSffun

--- TSffun.cxx
+++ TSffun.cxx
@@ -31,9 +31,13 @@ void TSffun::Evaluate(Double_t Q2, Double_t w2, Double_t t)
     Double_t M_p = kMassProton;
     Double_t m = kMassDetectedHadron;
     Double_t M_u = kMassUndetectedHadron;
+
+    Double_t Sx = w2 + Q2 - M_p * M_p;
     Double_t SqrtW2 = Sqrt(w2);
-    Double_t Sx_t = fInv->Sx() + t + M_p * M_p - M_u*M_u;
+    Double_t LambdaQ = Sx * Sx + 4 * M_p * M_p * Q2;
+    Double_t Sx_t = Sx + t + M_p * M_p - M_u*M_u;
     Double_t tq = t + Q2 - m * m;
+
     Double_t sffun_cmp = Power((w2 - M_u*M_u - m * m), 2) - 4 * M_u*M_u * m * m;
 
     if (sffun_cmp < 0)
@@ -41,14 +45,15 @@ void TSffun::Evaluate(Double_t Q2, Double_t w2, Double_t t)
     
 
     Double_t SqrtLw = Sqrt(Max(0., sffun_cmp));
-    Double_t ssffun_cmp = Q2 * Power(Sx_t, 2) - Sx_t * fInv->Sx() * tq - M_p * M_p * Power(tq, 2) - m * m * fInv->LambdaQ();
 
-    if (ssffun_cmp < 0)
-        std::cout << "ssffun: qll=NaN " << ssffun_cmp << std::endl;
+    Double_t ssffun_cmp2 = Q2 * Power(Sx_t, 2) - Sx_t * Sx * tq - M_p * M_p * Power(tq, 2) - m * m * LambdaQ;
+
+    if (ssffun_cmp2 < 0)
+        std::cout << "ssffun: qll=NaN " << ssffun_cmp2 << std::endl;
     
 
-    Double_t SqrtLl = Sqrt(Max(0., ssffun_cmp));
-    Double_t cspion = (2 * tq * w2 + (fInv->Sx() - 2 * Q2) * (w2 + m * m - M_u*M_u)) / SqrtLw / Sqrt(fInv->LambdaQ());
+    Double_t SqrtLl = Sqrt(Max(0., ssffun_cmp2));
+    Double_t cspion = (2 * tq * w2 + (Sx - 2 * Q2) * (w2 + m * m - M_u*M_u)) / SqrtLw / Sqrt(LambdaQ);
 
 //  Exclusive peak model (cross sections sigma_L,T,LT... from MAID2003)
 
