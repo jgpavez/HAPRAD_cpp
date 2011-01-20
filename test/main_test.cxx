ab189855e7c15dbf19834bda5f7c490898ebd372
smancill <smancill@alumnos.inf.utfsm.cl>
2011-01-20 17:59:47 -0300

    Print RC factor to stdout

--- test/main_test.cxx
+++ test/main_test.cxx
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "haprad_constants.h"
 #include "TRadCor.h"
+#include <iostream>
 
 
 int main(int argc, char *argv[])
@@ -9,6 +10,8 @@ int main(int argc, char *argv[])
     TRadCor rc;
     Double_t m = TMath::Power((kMassNeutron + kMassPion),2);
     rc.SetParameters(6,0.32,2.5,0.1, -4.6002448552,140,m);
-    rc.GetRCFactor();
+    Double_t factor = rc.GetRCFactor();
+
+    std::cout << std::endl << "RC factor: " << factor << std::endl;
     return 0;
 }
