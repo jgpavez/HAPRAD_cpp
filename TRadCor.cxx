b36c903148675c9766754c74f9083afe402ba672
smancill <smancill@alumnos.inf.utfsm.cl>
2011-01-24 17:37:14 -0300

    Add documentation to methods of the main class

--- TRadCor.cxx
+++ TRadCor.cxx
@@ -86,6 +86,15 @@ void TRadCor::CalculateRCFactor(Double_t E, Double_t x, Double_t Q2,
                                 Double_t z, Double_t p_t, Double_t phi,
                                 Double_t maxMx2)
 {
+    // Calculate, without returning it, the radiative correction factor for
+    // the given parameters. You can use then one of the three methods:
+    // GetFactor1, GetFactor2 and GetFactor3 to get the value you want.
+    //
+    // The E parameter is the energy of the beam, the x, Q2, z, p_t and phi
+    // are the values of the kinematical variables who describe the cross
+    // section of hadron electroproduction, and maxMx2 is the maximum amount
+    // of missing mass.
+
     CreateVariables(E,x,Q2,z,p_t,phi);
     rc = 0;
 
@@ -112,7 +121,11 @@ void TRadCor::CalculateRCFactor(Double_t E, Double_t x, Double_t Q2,
 Double_t TRadCor::GetRCFactor(Double_t E, Double_t x, Double_t Q2, Double_t z,
                               Double_t p_t, Double_t phi, Double_t maxMx2)
 {
-    // Get the radiative correction factor for the given parameters.
+    // Calculate and return the default radiative correction factor for the
+    // given parameters.
+    //
+    // You can get the same value first calling the CalculateRCFactor(...)
+    // method and then GetFactor2().
     //
     // The E parameter is the energy of the beam, the x, Q2, z, p_t and phi
     // are the values of the kinematical variables who describe the cross
@@ -127,6 +140,9 @@ Double_t TRadCor::GetRCFactor(Double_t E, Double_t x, Double_t Q2, Double_t z,
 
 Double_t TRadCor::GetFactor1(void)
 {
+    // Return the radiative correction factor without the exclusive radiative
+    // tail contribution. You need to call CalculateRCFactor(...) first.
+
     Double_t sigma_obs_f1 = sig_obs + tai[0];
 
     return sigma_obs_f1 / sigma_born;
@@ -136,6 +152,11 @@ Double_t TRadCor::GetFactor1(void)
 
 Double_t TRadCor::GetFactor2(void)
 {
+    // Return the radiative correction factor with the exclusive radiative
+    // tail contribution. You need to call CalculateRCFactor(...) first.
+    //
+    // This is the default value, returned by GetRCFactor(...).
+
     Double_t sigma_obs_f2 = sig_obs + tai[0] + tai[1];
 
     return sigma_obs_f2 / sigma_born;
@@ -145,6 +166,10 @@ Double_t TRadCor::GetFactor2(void)
 
 Double_t TRadCor::GetFactor3(void)
 {
+    // Return the radiative correction factor with the exclusive radiative
+    // tail contribution divided by 2. You need to call CalculateRCFactor(...)
+    // first.
+
     Double_t sigma_obs_f3 = sig_obs + tai[0] + tai[1] / 2;
 
     return sigma_obs_f3 / sigma_born;
